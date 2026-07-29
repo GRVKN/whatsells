875e518114376acc599b9f7462f956743f1c1f8f
GRVKN <whitedragon0988@gmail.com>
2026-07-29 11:23:04 +0200

    Add files via upload

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,16 @@
-/**
- * This is intended to be a basic starting point for linting in your app.
- * It relies on recommended configs out of the box for simplicity, but you can
- * and should modify this configuration to best suit your team's needs.
- */
-
-/** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+  ],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
@@ -14,83 +18,21 @@ module.exports = {
       jsx: true,
     },
   },
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
+  settings: {
+    react: {
+      version: "detect",
+    },
   },
-  ignorePatterns: ["!**/.server", "!**/.client"],
-
-  // Base config
-  extends: ["eslint:recommended"],
-
-  overrides: [
-    // React
-    {
-      files: ["**/*.{js,jsx,ts,tsx}"],
-      plugins: ["react", "jsx-a11y"],
-      extends: [
-        "plugin:react/recommended",
-        "plugin:react/jsx-runtime",
-        "plugin:react-hooks/recommended",
-        "plugin:jsx-a11y/recommended",
-      ],
-      settings: {
-        react: {
-          version: "detect",
-        },
-        formComponents: ["Form"],
-        linkComponents: [
-          { name: "Link", linkAttribute: "to" },
-          { name: "NavLink", linkAttribute: "to" },
-        ],
-        "import/resolver": {
-          typescript: {},
-        },
+  rules: {
+    "no-empty": "off",
+    "no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
       },
-      rules: {
-        "react/no-unknown-property": ["error", { ignore: ["variant"] }],
-      },
-    },
-
-    // Typescript
-    {
-      files: ["**/*.{ts,tsx}"],
-      plugins: ["@typescript-eslint", "import"],
-      parser: "@typescript-eslint/parser",
-      settings: {
-        "import/internal-regex": "^~/",
-        "import/resolver": {
-          node: {
-            extensions: [".ts", ".tsx"],
-          },
-          typescript: {
-            alwaysTryTypes: true,
-          },
-        },
-      },
-      extends: [
-        "plugin:@typescript-eslint/recommended",
-        "plugin:import/recommended",
-        "plugin:import/typescript",
-      ],
-    },
-
-    // Node
-    {
-      files: [
-        ".eslintrc.cjs",
-        "vite.config.{js,ts}",
-        ".graphqlrc.{js,ts}",
-        "shopify.server.{js,ts}",
-        "**/*.server.{js,ts}",
-      ],
-      env: {
-        node: true,
-      },
-    },
-  ],
-  globals: {
-    shopify: "readonly"
+    ],
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
   },
 };
